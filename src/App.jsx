f18a779ae482c98484614d318920c9dc234273f6
Nhan-macmini <ltn.sunrise@gmail.com>
2022-12-28 21:26:08 +0700

    tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -19,9 +19,17 @@ const App = () => {
     <BrowserRouter>
       <AdoptedPetContextProvider>
         <QueryClientProvider client={queryClient}>
-          <div>
-            <header>
-              <Link to="/">Adopt Me!</Link>
+          <div
+            className="p-0 m-0"
+            style={{
+              background:
+                "url(http://pets-images.dev-apis.com/pets/wallpaperA.jpg)",
+            }}
+          >
+            <header className="w-full mb-10 text-center p-7 bg-gradient-to-b from-yellow-400 via-orange-500 to-red-500">
+              <Link className="text-6xl text-white hover:text-gray-200" to="/">
+                Adopt Me!
+              </Link>
             </header>
             <Routes>
               <Route path="/details/:id" element={<Details />} />
--- src/components/SearchParams.jsx
+++ src/components/SearchParams.jsx
@@ -22,6 +22,7 @@ const SearchParams = () => {
   return (
     <div className="search-params">
       <form
+        className="p-10 mb-10 rounded-lg bg-gray-200 shadow-lg flex flex-col justify-center items-center"
         onSubmit={(e) => {
           e.preventDefault();
           const formData = new FormData(e.target);
@@ -45,7 +46,7 @@ const SearchParams = () => {
 
         <label htmlFor="animal">
           Animal
-          <select id="animal" name="animal">
+          <select id="animal" name="animal" className="search-input">
             <option />
             {ANIMALS.map((animal) => (
               <option key={animal} value={animal}>
@@ -57,7 +58,12 @@ const SearchParams = () => {
 
         <label htmlFor="breed">
           Breed
-          <select disabled={!breeds.length} id="breed" name="breed">
+          <select
+            disabled={!breeds.length}
+            id="breed"
+            name="breed"
+            className="search-input grayed-out-disabled"
+          >
             <option />
             {breeds.map((breed) => (
               <option key={breed} value={breed}>
@@ -66,7 +72,9 @@ const SearchParams = () => {
             ))}
           </select>
         </label>
-        <button>Submit</button>
+        <button className="rounded px-6 py-2 text-white hover:opacity-50 border-none bg-orange-500">
+          Submit
+        </button>
       </form>
       <Results pets={pets} />
     </div>
